32f033670e8198633f5156f0f16bff616c91f783
Luca A Magalhaes <luca.a.magalhaes@gmail.com>
2018-07-30 17:28:06 -0300

    Adicionando documentacao para o projeto TTFrontend

--- ttfrontend/README.md
+++ ttfrontend/README.md
@@ -1,18 +1,29 @@
-# ttfrontend
+# TTFrontend
 
-> A Vue.js project
+> Projeto para pesquisa de Tweets na API do TTBackend
 
-## Build Setup
+A aplicação possui duas telas:
+
+* Tela inicial com a lista das ultimas pesquisas feitas e dos Trending Topics, com a opção de buscar usando uma dessas consultas ou iniciar uma nova consulta
+
+* Tela de consulta com o resultado da consulta e opção de realizar uma nova consulta
+
+## Instalacao e configuracao
+
+Antes de instalar o projeto, é necessário alterar o apontamento da API do TTBackend no arquivo:
+
+```
+src/api.js
+```
+Uma vez alterado o apontamento, basta executar os seguintes comandos:
 
 ``` bash
-# install dependencies
+# Para instalar dependencias
 npm install
 
-# serve with hot reload at localhost:8080
+# Para iniciar a aplicacao em 127.0.0.1:8080
 npm run dev
 
-# build for production with minification
+# Para gerar a build minificada para producao
 npm run build
 ```
-
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- ttfrontend/src/api.js
+++ ttfrontend/src/api.js
@@ -1,42 +1,61 @@
 import axios from 'axios'
 
+// URL to access the TTBackend project
+// update this url to match your TTBackend setup
+const host = 'http://ttbackend/'
+
+// Retrieve the tweets that match the provided query
 export const ApiSearch = (query, store) => {
+    // Send the request to the API
     const params = new URLSearchParams();
     params.append('query', query);
-    axios.post('http://ttbackend/index.php/api/search/', params)
+    axios.post(host+'index.php/api/search/', params)
     .then((response) => {
+        // Fetch the data from the response
         return response.data
     })
     .then((data) => {
+        // Dispatch the returned tweets to the store
         store.dispatch("setTweets", data);
     })
+    // Log error
     .catch((err) => {
         console.log(err);
     });
 }
 
+// Retrieve the recent searched querys
 export const ApiRecent = (store) => {
-    axios.get('http://ttbackend/index.php/api/recent/')
+    // Send the request to the API
+    axios.get(host+'index.php/api/recent/')
     .then((response) => {
+        // Fetch the data from the response
         return response.data
     })
     .then((data) => {
+        // Dispatch the returned querys to the store
         store.dispatch("setRecent", data);
     })
     .catch((err) => {
+        // Log error
         console.log(err);
     });
 }
 
+// Retrieve the trending querys
 export const ApiTrending = (store) => {
-    axios.get('http://ttbackend/index.php/api/trending/')
+    // Send the request to the API
+    axios.get(host+'index.php/api/trending/')
     .then((response) => {
+        // Fetch the data from the response
         return response.data
     })
     .then((data) => {
+        // Dispatch the returned querys to the store
         store.dispatch("setTrending", data);
     })
     .catch((err) => {
+        // Log error
         console.log(err);
     });
 }
